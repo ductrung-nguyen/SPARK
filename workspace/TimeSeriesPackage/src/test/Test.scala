327e56b4aeee535400900bf0ac07d19c7838a790
Nguyen Duc Trung <loveallufev@ubuntu.(none)>
2014-01-21 02:31:12 +0100

    add feature evaluate

--- workspace/TimeSeriesPackage/src/test/Test.scala
+++ workspace/TimeSeriesPackage/src/test/Test.scala
@@ -22,11 +22,17 @@ object Test {
 	    
 	    val bodyfat_data = context.textFile("data/bodyfat.csv", 1)
 	    val bodyfat_metadata = context.textFile("data/bodyfat.tag", 1)
-	    val tree2 = new RegressionTree(bodyfat_data, bodyfat_metadata, context,2)
+	    
+	    var i = -1; 
+	    val training_bodyfat = bodyfat_data.keyBy(x => {i = i + 1; i}).filter(x => x._1 <50).map(x => x._2)
+	    val evaluate_bodyfat = bodyfat_data.keyBy(x => {i = i + 1; i}).filter(x => x._1 >= 50).map(x => x._2)
+	    
+	    val tree2 = new RegressionTree(training_bodyfat, bodyfat_metadata, context,2)
 	    stime = System.nanoTime()
 	    println(tree2.buildTree("DEXfat", Set("age", "waistcirc","hipcirc","elbowbreadth","kneebreadth")))
 	    println("Build tree in %f second(s)".format((System.nanoTime() - stime)/1e9))
-	    
-	    
+	    println("Predict:" + tree2.predict("53,56,29.83,81,103,6.9,8.9,4.14,4.52,4.31,5.69".split(",")))
+	    // evaluate with training set
+	    tree2.evaluate(evaluate_bodyfat)
 	}
 }
