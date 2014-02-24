0fc80fd976dc4943c9e1696e6cbb157968a09f35
xsha <xiaolan.sha@gmail.com>
2014-02-24 15:53:31 +0100

    code structure clean

--- workspace/TimeSeriesPackage/.worksheet/src/spark.WhyNot.scala
+++
@@ -1,7 +0,0 @@
-package spark
-
-object WhyNot{;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(57); 
-
-  val list = List(1,2,3,4);System.out.println("""list  : List[Int] = """ + $show(list ));$skip(29); 
-	val list2 = l.updated(2, 5);System.out.println("""list2  : <error> = """ + $show(list2 ))}
-}
--- workspace/TimeSeriesPackage/.worksheet/src/test.test6.scala
+++
@@ -1,5 +0,0 @@
-package test
-
-object test6 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(72); 
-  println("Welcome to the Scala worksheet")}
-}
